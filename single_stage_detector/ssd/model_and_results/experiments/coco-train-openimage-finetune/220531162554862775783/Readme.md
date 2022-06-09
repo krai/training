0d90bd9e1fef2913290627745d45e55382c69bf9
elim <elim@krai.ai>
2022-06-09 10:45:32 +0100

    update gitignore to add logs

---
+++ single_stage_detector/ssd/model_and_results/experiments/coco-train-openimage-finetune/220531162554862775783/Readme.md
@@ -0,0 +1,5 @@
+Results of finetuning with openimages on coco-trained model. 
+Epoch 0: Achieved 0.16 accuracy. 
+Epoch 1: Failed at eval accuracy steps.
+Epoch 2: Reusmed from Epoch 1. Accuracy achieved is worst than that of Epoch 0, loss is increasing.
+Epoch 3: Training failed mid-way becuase of nan values in loss. 
---
+++ single_stage_detector/ssd/model_and_results/experiments/openimage-train-coco-finetune/220606192017545931369/config_SUT.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+## DL params
+export BATCHSIZE=4
+export NUMEPOCHS=8
+export DATASET_DIR="/datasets/open-images-v6-mlperf"
+export EXTRA_PARAMS='--lr 0.0001 --output-dir=/results --dataset=coco --resume=/model/openimage_model_10.pth --resume-from-diff-dataset'
+
+## System run parms
+export DGXNNODES=1
+export DGXSYSTEM=$(basename $(readlink -f ${BASH_SOURCE[0]}) | sed 's/^config_//' | sed 's/\.sh$//' )
+export WALLTIME=08:00:00
+
+## System config params
+export DGXNGPU=2
+export DGXSOCKETCORES=24
+export DGXNSOCKET=1
+export DGXHT=2  # HT is on is 2, HT off is 1
